e578466231664f98ef45e3becf248743be024be8
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2021-12-30 00:13:24 +0100

    Added the Items feild

--- aot_scraper/aot_scraper/spiders/aot_locations.py
+++ aot_scraper/aot_scraper/spiders/aot_locations.py
@@ -4,8 +4,34 @@ import scrapy
 class AotLocationsSpider(scrapy.Spider):
     name = 'aot_locations'
     allowed_domains = ['attackontitan.fandom.com']
-    start_urls = ['http://attackontitan.fandom.com/']
+    start_urls = ['https://attackontitan.fandom.com/wiki/Category:Locations_(Anime)']
 
     def parse(self, response):
+        """
+        Function for looping through the location entries and retrieving links
+        :params self
+        :params response
+        """
+        for entry in response.css("div.category-page__members-wrapper"):
+            location_link = entry.css("a.category-page__member-link").attrib["href"]
+
+
+            request = scrapy.follow(location_link, callback=self.article_reader)
+
+            request.meta['item'] = location_link
+
+            yield request
+
+
+    @staticmethod
+    def article_reader(response):
+        """
+        The scraper which gets the data from individual pages
+        :params response
+        :return generator
+        """
+
+
+
         pass
  
