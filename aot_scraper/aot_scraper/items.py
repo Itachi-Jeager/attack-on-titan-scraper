204a4fbb48521db1ad85c46acd0e5f8f403ac403
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2022-01-06 23:36:21 +0100

    Location Item Loader

--- aot_scraper/aot_scraper/items.py
+++ aot_scraper/aot_scraper/items.py
@@ -4,9 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import Join, TakeFirst, MapCompose
 
 
-class AotScraperItem(scrapy.Item):
+class AotLocationItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    source = scrapy.Field()
+    name = scrapy.Field()
+    rel_location = scrapy.Field()
+    residents = scrapy.Field()
+
