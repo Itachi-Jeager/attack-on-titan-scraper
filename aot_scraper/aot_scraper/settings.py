8d7f08c50970b6de6b255b6038cb025788aa82cf
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2022-01-06 23:57:32 +0100

    Updated settings to use scrapy-deltafetch

--- aot_scraper/aot_scraper/settings.py
+++ aot_scraper/aot_scraper/settings.py
@@ -19,16 +19,22 @@ NEWSPIDER_MODULE = 'aot_scraper.spiders'
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
+# Use UTF-8 encoding
+FEED_EXPORT_ENCODING = 'utf-8'
+
+# Fields to export with
+FEED_EXPORT_FIELDS = ["source", "name", "rel_location", "residents", ]
+
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 5
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 8
+CONCURRENT_REQUESTS_PER_IP = 8
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
@@ -44,9 +50,12 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'aot_scraper.middlewares.AotScraperSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+   'aot_scraper.middlewares.AotScraperSpiderMiddleware': 543,
+   'scrapy_deltafetch.DeltaFetch': 10,
+}
+
+DELTAFETCH_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
