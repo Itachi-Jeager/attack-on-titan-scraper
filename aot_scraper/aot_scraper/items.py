bc71008bc79c7aa039f42ac2175bfd1ce9e75c0a
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2022-01-07 01:28:40 +0100

    experimenting with input processor

--- aot_scraper/aot_scraper/items.py
+++ aot_scraper/aot_scraper/items.py
@@ -5,12 +5,18 @@
 
 import scrapy
 from itemloaders.processors import Join, TakeFirst, MapCompose
+from w3lib.html import remove_tags
+
+#TODO: Comment this file properly
+#TODO: Remove input processor from source and confirm that it still works
+#TODO: Preprocess the fields based on the received data
 
 
 class AotLocationItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    source = scrapy.Field()
+    source = scrapy.Field(input_processor=MapCompose(remove_tags),
+                         output_processor=TakeFirst())
     name = scrapy.Field()
     rel_location = scrapy.Field()
     residents = scrapy.Field()
