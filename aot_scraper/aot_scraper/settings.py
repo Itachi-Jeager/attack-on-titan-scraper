9716b686cbde0a94440d5a5414d8925338cb8331
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2022-01-07 01:30:27 +0100

    experimenting with settings

--- aot_scraper/aot_scraper/settings.py
+++ aot_scraper/aot_scraper/settings.py
@@ -69,11 +69,13 @@ DELTAFETCH_ENABLED = True
 #    'scrapy.extensions.telnet.TelnetConsole': None,
 #}
 
+#TODO: Comment out Item Pipelines and Check the effect
+
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'aot_scraper.pipelines.AotScraperPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'aot_scraper.pipelines.AotScraperPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
