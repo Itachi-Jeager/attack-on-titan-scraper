a2af92ee8fd2a5c294ba5517c278235043c82801
Oluwatobi Ogunware <oluwatobi.ogunware@e4email.net>
2022-01-07 23:07:02 +0100

    basic items

--- aot_scraper/aot_scraper/items.py
+++ aot_scraper/aot_scraper/items.py
@@ -3,21 +3,17 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
 
+# imports
 import scrapy
 from itemloaders.processors import Join, TakeFirst, MapCompose
 from w3lib.html import remove_tags
 
-#TODO: Comment this file properly
-#TODO: Remove input processor from source and confirm that it still works
-#TODO: Preprocess the fields based on the received data
+# TODO: Preprocess the fields based on the received data
 
 
 class AotLocationItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    source = scrapy.Field(input_processor=MapCompose(remove_tags),
-                         output_processor=TakeFirst())
+
+    source = scrapy.Field()
     name = scrapy.Field()
     rel_location = scrapy.Field()
     residents = scrapy.Field()
-
